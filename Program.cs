e30d01f0430856a483bc60066470537020509688
Lang Hall <lhall@heckgrammar.co.uk>
2024-12-17 11:18:11 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Globalization;
+
+namespace validation_and_verification_
+{
+    internal class Program
+    {
+        static int rangeCheck(int age)
+        {
+            while (!(age >= 0 && age <= 150))
+            {
+                Console.WriteLine("age not in range please re enter a valid age ");
+                Console.WriteLine("enter your age>>");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Range check validated");
+            return age;
+        }
+        static void lengthCheck(string name)
+        {
+            if (name.Length>= 2 && name.Length<= 748)
+            {
+                Console.WriteLine("Lenth of name is valid");
+            }
+            else
+            {
+                Console.WriteLine("Length of name is not valid");
+            }
+        }
+        static void TypeCheck(string name) 
+        {
+            if (name is string) 
+            {
+                Console.WriteLine("The inputted name is a string data type");
+            }
+        }
+
+        static string PresenceCheck(string name)
+        {
+            while (name == "")
+            {
+                Console.WriteLine("Name was left blank");
+                Console.WriteLine("Enter your name>>");
+                name = Console.ReadLine();
+            }
+            return name;
+        }
+        static bool LogIn(string username, string password) 
+        {
+            if (username == "Mr Milton" && password == "Pass1234")
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name>>");
+            string name= Console.ReadLine();
+            Console.WriteLine("enter your age>>");
+            int age = Convert.ToInt32(Console.ReadLine());
+            age = rangeCheck(age);
+            TypeCheck(name);
+            name = PresenceCheck(name);
+            lengthCheck(name);
+            string username = name;
+            Console.WriteLine("enter your password>>");
+            string password = Console.ReadLine();
+            bool verfied = LogIn(username, password);
+            while (!verfied) 
+            {
+                Console.WriteLine("Enter your username>>");
+                username = Console.ReadLine();
+                Console.WriteLine("enter your password>>");
+                password = Console.ReadLine();
+                verfied = LogIn(username, password);
+            }
+
+        }
+    }
+}
